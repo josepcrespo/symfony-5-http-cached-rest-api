668252f59d62ecf6e665c458cfc1bb9f9cda8366
Josep Crespo Santacreu <josep.crespo.santacreu@gmail.com>
2021-12-26 20:10:46 +0000

    composer require --dev symfony/maker-bundle

--- config/bundles.php
+++ config/bundles.php
@@ -16,5 +16,5 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
-    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
 ];
