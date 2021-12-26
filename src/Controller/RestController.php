3877facf46571248a1588c7cde2fba14438d65a5
Josep Crespo Santacreu <josep.crespo.santacreu@gmail.com>
2021-12-26 20:51:33 +0000

    Adding validations into the RestController.

--- src/Controller/RestController.php
+++ src/Controller/RestController.php
@@ -2,14 +2,15 @@
 
 namespace App\Controller;
 
+use App\Exception\ResourceNotFoundException;
+use Doctrine\ORM\EntityManagerInterface;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use FOS\RestBundle\View\View;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\Request;
-use Doctrine\ORM\EntityManagerInterface;
 use Laminas\Hydrator\ReflectionHydrator;
-use App\Exception\ResourceNotFoundException;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 /**
  * The rest api routes.
@@ -72,7 +73,11 @@ class RestController extends AbstractFOSRestController {
 	 * @return View
 	 * @Rest\Post("/{resource}")
 	 */
-	public function post(string $resource, Request $request): View {
+	public function post(
+		string $resource,
+		Request $request,
+		ValidatorInterface $validator
+	): View {
 		$args = $request->request->all();
 		if (!$args) {
 			$args = (array) json_decode($request->getContent());
@@ -80,6 +85,20 @@ class RestController extends AbstractFOSRestController {
 		$entity = new $resource();
 		$hydrator = new ReflectionHydrator();
 		$hydrator->hydrate($args, $entity);
+		
+		// Validation
+		$errors = $validator->validate($entity);
+		if (count($errors) > 0) {
+        /*
+         * Uses a __toString method on the $errors variable which is a
+         * ConstraintViolationList object. This gives us a nice string
+         * for debugging.
+         */
+        // $errorsString = (string) $errors;
+
+        return View::create($errors, Response::HTTP_BAD_REQUEST);
+    }
+
 		$this->manager->persist($entity);
 		$this->manager->flush();
 		return View::create($entity, Response::HTTP_OK);
@@ -94,7 +113,12 @@ class RestController extends AbstractFOSRestController {
 	 * @return View
 	 * @Rest\Put("/{resource}/{id}")
 	 */
-	public function put(string $resource, string $id, Request $request): View {
+	public function put(
+		string $resource,
+		string $id,
+		Request $request,
+		ValidatorInterface $validator
+	): View {
 		$repository = $this->manager->getRepository($resource);
 		$entity = $repository->findOneBy(['id' => $id]);
 		if (!$entity) {
@@ -106,6 +130,21 @@ class RestController extends AbstractFOSRestController {
 		}
 		$hydrator = new ReflectionHydrator();
 		$hydrator->hydrate($args, $entity);
+		
+		// Validation
+		$errors = $validator->validate($entity);
+		if (count($errors) > 0) {
+        /*
+         * Uses a __toString method on the $errors variable which is a
+         * ConstraintViolationList object. This gives us a nice string
+         * for debugging.
+         */
+        $errorsString = (string) $errors;
+
+        return View::create($errors, new Response($errorsString));
+    }
+
+		$this->manager->persist($entity);
 		$this->manager->flush();
 		return View::create($entity, Response::HTTP_OK);
 	}
