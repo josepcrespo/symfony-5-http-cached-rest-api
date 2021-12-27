ec4955364a6cedf2a481d3d6e1d70d2380a12dd0
Josep Crespo Santacreu <josep.crespo.santacreu@gmail.com>
2021-12-27 04:25:29 +0000

    composer require symfony/twig-pack twig/cssinliner-extra twig/inky-extra
    The PHP `xsl` extension is required by the `twig/inky-extra` dependency.

--- config/bundles.php
+++ config/bundles.php
@@ -17,4 +17,5 @@ return [
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
 ];
